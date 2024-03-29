24a07d68521739ae36aeb5fc65596033a6862af4
Thiago <prattithiago5@gmail.com>
2024-03-29 17:01:23 -0300

    ADD PRISMA CHECK IN REPOS

--- src/Repository/prisma/prisma-users.repos.ts
+++ src/Repository/prisma/prisma-users.repos.ts
@@ -4,16 +4,25 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepos implements UsersRepository {
-
-  findById(id: string): Promise<{ id: string; name: string; email: string; password_hash: string; created_at: Date } | null> {
-    throw new Error('Method not implemented.')
-  }
   
   async findByEmail(email: string) {
+
     const user = await prisma.user.findUnique({
       where: {
         email,
       },
+    })
+
+    return user
+  }
+
+  async findById(id: string) {
+    
+    const user = await prisma.user.findUnique({
+
+      where: {
+        id,
+      }
     })
 
     return user
