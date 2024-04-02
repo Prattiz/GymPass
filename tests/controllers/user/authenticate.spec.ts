eaa1926278034776427fd5a66eb3e59f65512303
Thiago <prattithiago5@gmail.com>
2024-04-02 10:17:02 -0300

    FIXING CODE AUTHENTICATE TEST

--- tests/controllers/user/authenticate.spec.ts
+++ tests/controllers/user/authenticate.spec.ts
@@ -1,17 +1,21 @@
-import request from 'supertest'
-import { app } from '@/app'
+import request from 'supertest';
+import { app } from '@/app';
+
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Authenticate (e2e)', () => {
+
   beforeAll(async () => {
     await app.ready()
-  })
+  });
 
   afterAll(async () => {
     await app.close()
-  })
+  });
+
 
   it('should be able to authenticate', async () => {
+
     await request(app.server).post('/users').send({
       name: 'Test Name',
       email: 'testemail@example.com',
@@ -19,13 +23,17 @@ describe('Authenticate (e2e)', () => {
     })
 
     const response = await request(app.server).post('/sessions').send({
+
       email: 'testemail@example.com',
       password: 'password12',
-    })
+    });
 
     expect(response.status).toEqual(200)
+
     expect(response.body).toEqual({
       token: expect.any(String),
     })
-  })
+
+  });
+
 })
